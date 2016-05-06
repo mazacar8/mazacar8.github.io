11efa75ed939b9aec7a1ee855f7ed227adf7bcd2
Preetam Amancharla <preetam.amancharla@gmail.com>
2016-05-05 21:31:34 -0400

    Graphics shows empty cube

--- main.cpp
+++ main.cpp
@@ -1,79 +1,121 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <math.h>
-#include "platformgl.h"
+/*
+ * OGL02Animation.cpp: 3D Shapes with animation
+ */
 
-// http://www.lighthouse3d.com/tutorials/glut-tutorial/animation/
-
-void changeSize(int w, int h) {
-
-  // Prevent a divide by zero, when window is too short
-  // (you cant make a window of zero width).
-  if (h == 0)
-    h = 1;
-
-  float ratio =  w * 1.0 / h;
-
-  // Use the Projection Matrix
-  glMatrixMode(GL_PROJECTION);
-
-  // Reset Matrix
-  glLoadIdentity();
-
-  // Set the viewport to be the entire window
-  glViewport(0, 0, w, h);
-
-  // Set the correct perspective.
-  gluPerspective(45.0f, ratio, 0.1f, 100.0f);
-
-  // Get Back to the Modelview
-  glMatrixMode(GL_MODELVIEW);
+#include "platformgl.h" 
+ 
+/* Global variables */
+char title[] = "3D Shapes with animation";
+GLfloat angleCube = 25.0f;     // Rotational angle for cube [NEW]
+int refreshMills = 15;        // refresh interval in milliseconds [NEW]
+ 
+/* Initialize OpenGL Graphics */
+void initGL() {
+   glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
+   glClearDepth(1.0f);                   // Set background depth to farthest
+   glEnable(GL_DEPTH_TEST);   // Enable depth testing for z-culling
+   glDepthFunc(GL_LEQUAL);    // Set the type of depth-test
+   glShadeModel(GL_SMOOTH);   // Enable smooth shading
+   glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);  // Nice perspective corrections
 }
+ 
+/* Handler for window-repaint event. Called back when the window first appears and
+   whenever the window needs to be re-painted. */
+void display() {
+   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear color and depth buffers
+   glMatrixMode(GL_MODELVIEW);     // To operate on model-view matrix
+ 
+   // Render a color-cube consisting of 6 quads with different colors
+   glLoadIdentity();                 // Reset the model-view matrix
+   glTranslatef(0.0f, 0.0f, -7.0f);  // Move right and into the screen
+   glRotatef(angleCube, 1.0f, 1.0f, 1.0f);  // Rotate about (1,1,1)-axis [NEW]
+   glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
 
-float angle = 0.0f;
-
-void renderScene(void) {
-
-  // Clear Color and Depth Buffers
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-  // Reset transformations
-  glLoadIdentity();
-  // Set the camera
-  gluLookAt(  0.0f, 0.0f, 10.0f,
-        0.0f, 0.0f,  0.0f,
-        0.0f, 1.0f,  0.0f);
-
-  glRotatef(angle, 0.0f, 1.0f, 0.0f);
-
-  glBegin(GL_TRIANGLES);
-    glVertex3f(-2.0f,-2.0f, 0.0f);
-    glVertex3f( 2.0f, 0.0f, 0.0);
-    glVertex3f( 0.0f, 2.0f, 0.0);
-  glEnd();
-
-  angle+=0.1f;
-
-  glutSwapBuffers();
+   glBegin(GL_QUADS);                // Begin drawing the color cube with 6 quads
+      // Top face (y = 1.0f)
+      // Define vertices in counter-clockwise (CCW) order with normal pointing out
+      //glColor3f(0.0f, 1.0f, 0.0f);     // Green
+      glVertex3f( 1.0f, 1.0f, -1.0f);
+      glVertex3f(-1.0f, 1.0f, -1.0f);
+      glVertex3f(-1.0f, 1.0f,  1.0f);
+      glVertex3f( 1.0f, 1.0f,  1.0f);
+ 
+      // Bottom face (y = -1.0f)
+      //glColor3f(1.0f, 0.5f, 0.0f);     // Orange
+      glVertex3f( 1.0f, -1.0f,  1.0f);
+      glVertex3f(-1.0f, -1.0f,  1.0f);
+      glVertex3f(-1.0f, -1.0f, -1.0f);
+      glVertex3f( 1.0f, -1.0f, -1.0f);
+ 
+      // Front face  (z = 1.0f)
+      //glColor3f(1.0f, 0.0f, 0.0f);     // Red
+      glVertex3f( 1.0f,  1.0f, 1.0f);
+      glVertex3f(-1.0f,  1.0f, 1.0f);
+      glVertex3f(-1.0f, -1.0f, 1.0f);
+      glVertex3f( 1.0f, -1.0f, 1.0f);
+ 
+      // Back face (z = -1.0f)
+      //glColor3f(1.0f, 1.0f, 0.0f);     // Yellow
+      glVertex3f( 1.0f, -1.0f, -1.0f);
+      glVertex3f(-1.0f, -1.0f, -1.0f);
+      glVertex3f(-1.0f,  1.0f, -1.0f);
+      glVertex3f( 1.0f,  1.0f, -1.0f);
+ 
+      // Left face (x = -1.0f)
+      //glColor3f(0.0f, 0.0f, 1.0f);     // Blue
+      glVertex3f(-1.0f,  1.0f,  1.0f);
+      glVertex3f(-1.0f,  1.0f, -1.0f);
+      glVertex3f(-1.0f, -1.0f, -1.0f);
+      glVertex3f(-1.0f, -1.0f,  1.0f);
+ 
+      // Right face (x = 1.0f)
+      //glColor3f(1.0f, 0.0f, 1.0f);     // Magenta
+      glVertex3f(1.0f,  1.0f, -1.0f);
+      glVertex3f(1.0f,  1.0f,  1.0f);
+      glVertex3f(1.0f, -1.0f,  1.0f);
+      glVertex3f(1.0f, -1.0f, -1.0f);
+   glEnd();  // End of drawing color-cube
+ 
+   glutSwapBuffers();  // Swap the front and back frame buffers (double buffering)
+ 
+   // Update the rotational angle after each refresh [NEW]
+   //angleCube -= 0.15f;
 }
-
-int main(int argc, char **argv) {
-
-  // init GLUT and create window
-  glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-  glutInitWindowPosition(100,100);
-  glutInitWindowSize(320,320);
-  glutCreateWindow("Lighthouse3D- GLUT Tutorial");
-
-  // register callbacks
-  glutDisplayFunc(renderScene);
-  glutReshapeFunc(changeSize);
-  glutIdleFunc(renderScene);
-
-  // enter GLUT event processing cycle
-  glutMainLoop();
-
-  return 1;
+ 
+/* Called back when timer expired [NEW] */
+void timer(int value) {
+   glutPostRedisplay();      // Post re-paint request to activate display()
+   glutTimerFunc(refreshMills, timer, 0); // next timer call milliseconds later
+}
+ 
+/* Handler for window re-size event. Called back when the window first appears and
+   whenever the window is re-sized with its new width and height */
+void reshape(GLsizei width, GLsizei height) {  // GLsizei for non-negative integer
+   // Compute aspect ratio of the new window
+   if (height == 0) height = 1;                // To prevent divide by 0
+   GLfloat aspect = (GLfloat)width / (GLfloat)height;
+ 
+   // Set the viewport to cover the new window
+   glViewport(0, 0, width, height);
+ 
+   // Set the aspect ratio of the clipping volume to match the viewport
+   glMatrixMode(GL_PROJECTION);  // To operate on the Projection matrix
+   glLoadIdentity();             // Reset
+   // Enable perspective projection with fovy, aspect, zNear and zFar
+   gluPerspective(45.0f, aspect, 0.1f, 100.0f);
+}
+ 
+/* Main function: GLUT runs as a console application starting at main() */
+int main(int argc, char** argv) {
+   glutInit(&argc, argv);            // Initialize GLUT
+   glutInitDisplayMode(GLUT_DOUBLE); // Enable double buffered mode
+   glutInitWindowSize(640, 480);   // Set the window's initial width & height
+   glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
+   glutCreateWindow(title);          // Create window with the given title
+   glutDisplayFunc(display);       // Register callback handler for window re-paint event
+   glutReshapeFunc(reshape);       // Register callback handler for window re-size event
+   initGL();                       // Our own OpenGL initialization
+   glutTimerFunc(0, timer, 0);     // First timer call immediately [NEW]
+   glutMainLoop();                 // Enter the infinite event-processing loop
+   return 0;
 }
